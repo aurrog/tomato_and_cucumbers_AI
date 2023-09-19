a9e2ef01fa456a02544f3849b6953cd5b8e0f23b
aurrog <gnusovme@gmail.com>
2023-09-19 20:08:11 +0300

    Add 2 files

--- image_to_array.py
+++ image_to_array.py
@@ -1,10 +1,15 @@
 from PIL import Image
 import numpy as np
+import os
 
-
-img = Image.open('../Cucumber/Image_1.jpg')
+img = Image.open('Cucumber/Image_1.jpg')
 img = img.resize((90, 90))
 img = img.convert('L')
 # img.show()
 arr = np.asarray(img, dtype='uint8')
 print(arr.shape)
+# Заппустить в цикл и сохранить сброку массивов в json
+path = 'Tomato'
+for root, dirs, files in os.walk(path):
+    for filename in files:
+        print(path+'/'+filename)
